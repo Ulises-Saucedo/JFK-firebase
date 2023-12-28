0ead096dec320a8fd03857a1a11a2a8500858e71
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2023-12-28 15:28:34 -0300

    dependencies updates and gallery view added

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,12 +1,45 @@
 <script setup>
-    import HeaderLayout from '../layouts/HeaderLayout.vue';
+    import { onMounted, onUnmounted } from 'vue';
+    import { images } from '../helpers/images.js'
+    import HeaderLayout from '../layouts/HeaderLayout.vue'
+    import PhotoSwipeLightbox from 'photoswipe/lightbox'
+    import 'photoswipe/style.css'
+
+    let lightbox = null;
+
+
+    onMounted(() =>{
+        if (!lightbox) {
+            lightbox = new PhotoSwipeLightbox({
+                gallery: '#' + "my-test-gallery",
+                children: 'a',
+                pswpModule: () => import('photoswipe'),
+            })
+            lightbox.init();
+        }
+    })
+
+    onUnmounted(() =>{
+        if (lightbox) {
+            lightbox.destroy();
+            lightbox = null;
+        }
+    })
 </script>
 
 <template>
     <HeaderLayout></HeaderLayout>
-    <h1>GALERIA</h1>
+    <div id="my-test-gallery" class="flex flex-wrap p-5 justify-center mx-auto gap-5">
+        <a
+        v-for="(image, key) in images"
+        :key="key"
+        :href="image.largeURL"
+        :data-pswp-width="image.width"
+        :data-pswp-height="image.height"
+        target="_blank"
+        rel="noreferrer"
+        >
+            <img :src="image.thumbnailURL" :alt="image.alt" class="w-[350px] h-[250px] gap-5"/>
+        </a>
+    </div>
 </template>
-
-<style scoped>
-
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
-    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin'),
     require("daisyui")
   ],
 }
