a3d9ec2dab30e485c535763d868a7d962c042fca
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2023-12-29 22:01:25 -0300

    firebase dependency added

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router/router.js'
 import VueKinesis from "vue-kinesis"
 import VueRoughNotation from 'vue-rough-notation'
+import { initializeApp } from "firebase/app"
 import { OhVueIcon, addIcons } from "oh-vue-icons"
 import { 
     CoHome, 
@@ -35,6 +36,17 @@ addIcons(
     BiBank,
     BiEnvelopePaper,
     MdFolderspecialOutlined
+)
+
+initializeApp(
+    {
+        apiKey: import.meta.env.VITE_API_KEY,
+        authDomain: "jfk-firebase.firebaseapp.com",
+        projectId: "jfk-firebase",
+        storageBucket: "jfk-firebase.appspot.com",
+        messagingSenderId: "35158972095",
+        appId: "1:35158972095:web:7048e869f5f8ee9898180a"
+    }
 )
 
 createApp(App).use(router).use(VueKinesis).use(VueRoughNotation).component("v-icon", OhVueIcon).mount('#app')
